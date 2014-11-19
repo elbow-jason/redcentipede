ccd789c2d00a051204854c062d3c67362e890078
jlgoldb2 <jlgoldb2@asu.edu>
2014-11-18 23:46:08 -0700

    Agent and Point structs and fn travel() are compiling

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,56 @@
 
 
 fn main() {
-    let first_agent = Agent {
-        x: 0,
-        y: 0,
+    let mut first_agent = Agent {
+        location: Point {
+            x: 0,
+            y: 0,
+        },
         reserves: 100,
         chromosomes: vec!["ABCD1234".to_string(), "11100101".to_string()],
     };
 
-    let some_resource = Resource {
-        x: 10,
-        y: 10,
+    let mut some_resource = Resource {
+        location: Point {
+            x: 10,
+            y: 10,
+        },
         value: 100,
-    }; 
+    };
 
     println!("\n")
-    println!("I am first_agent my (x, y) is ({}, {})", first_agent.x, first_agent.y);
+    println!("I am first_agent my (x, y) is ({}, {})", first_agent.location.x, first_agent.location.y);
     println!("I have {} resources.", first_agent.reserves);
     println!("My chromosomes are {}.", first_agent.chromosomes);
     println!("\n")
-    println!("I am some_resource my (x, y) is ({}, {})", some_resource.x, some_resource.y);
+    println!("I am some_resource my (x, y) is ({}, {})", some_resource.location.x, some_resource.location.y);
     println!("I have {} resources.", some_resource.value);
 }
 
-struct Agent  {
-    x: int,
-    y: int,
+pub struct Point {
+    pub y: int,
+    pub x: int,
+}
+
+pub struct Agent  {
+    pub location: Point,
     reserves: int,
     chromosomes: Vec<String>,
 }
 
-struct Resource {
-    x: int,
-    y: int,
-    value: int,
+pub struct Resource {
+    pub location: Point,
+    pub value: int,
+}
+
+trait Mobile {
+    fn travel(&mut self, delta_x: int, delta_y: int);
 }
 
 
-
-
+impl Mobile for Agent {
+    fn travel(&mut self, delta_x: int, delta_y: int) {
+        self.location.x = *&self.location.x + delta_x;
+        self.location.y = *&self.location.y + delta_y;
+    }
+}
